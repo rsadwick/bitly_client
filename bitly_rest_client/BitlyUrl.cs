afa2a1d42237ce7d085fcd1248f11599d2b60378
Ryan Sadwick <rsadwick@gmail.com>
2015-01-16 10:17:48 -0500

    added project

---
+++ bitly_rest_client/BitlyUrl.cs
@@ -0,0 +1,11 @@
+﻿namespace bitly_rest_client {
+    public class BitlyUrl {
+        public string status_code { get; set; }
+        public data data { get; set; }
+    }
+
+    public class data {
+        public string long_url { get; set; }
+        public string url { get; set; }
+    }
+}
